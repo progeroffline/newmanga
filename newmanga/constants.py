12ecb0af9b9bbfb3ff8be49903e34f17d7a5d945
progeroffline <progeroffline@gmail.com>
2024-08-18 23:18:03 +0300

    Create endpoint for chapters

--- newmanga/constants.py
+++ newmanga/constants.py
@@ -9,6 +9,7 @@ catalogue = f"https://{api_domain}/catalogue"
 manga = f"https://{web_domain}/p"
 comments = "https://" + api_v2_domain + "/v2/projects/{slug}/comments"
 similar = "https://" + api_v2_domain + "/v2/projects/{slug}/similar"
+chapters = "https://" + api_v2_domain + "/v2/branches/{id}/chapters"
 manga_api = f"https://{api_v2_domain}/v2/projects"
 
 image_storage_url = f"https://{images_storage_domain}/ProjectCard/webp"
